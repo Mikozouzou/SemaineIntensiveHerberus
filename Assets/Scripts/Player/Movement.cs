62baeed8a1f48045ba0cbec3f2378f661bf81301
William <w.patruno@rubika-edu.com>
2016-05-12 19:12:54 +0200

    Fx player et impact

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -21,6 +21,7 @@ public class Movement : MonoBehaviour {
         anim.Play();
         transform.FindChild("Player_Physics").gameObject.layer = 8;
         //Physics.IgnoreLayerCollision(8, 9);
+        
     }
 	
 	
