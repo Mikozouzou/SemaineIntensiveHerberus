f4ab5a5602db15df7d03f82b1c6a8f7b13b80af1
haerod <elie.buglione@gmail.com>
2016-05-13 17:11:38 +0200

    Credits + Music

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour
+{
+	
+	void Update ()
+    {
+	    if (XInput.instance.getButton(1,'B') == XInputDotNetPure.ButtonState.Pressed || XInput.instance.getButton(1, 'A') == XInputDotNetPure.ButtonState.Pressed)
+        {
+            //Application.LoadLevel("lala");
+        }
+	}
+}
