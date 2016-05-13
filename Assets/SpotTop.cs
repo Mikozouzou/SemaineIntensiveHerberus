2ebb73af3ec78a5b351bc20ac9f3b5baa11b85a4
William <w.patruno@rubika-edu.com>
2016-05-13 10:36:16 +0200

    Spotlight

--- Assets/SpotTop.cs
+++ Assets/SpotTop.cs
@@ -2,14 +2,14 @@
 using System.Collections;
 
 public class SpotTop : MonoBehaviour {
-
+    public Transform trophy;
 	// Use this for initialization
 	void Start () {
-	
+        trophy = GameObject.FindGameObjectWithTag("Trophy").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        transform.LookAt(trophy);
 	}
 }
