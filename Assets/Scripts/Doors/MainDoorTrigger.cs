76d43f2559fb34a114125927850e546039c47a57
William <w.patruno@rubika-edu.com>
2016-05-13 13:47:39 +0200

    Light Corrected

--- Assets/Scripts/Doors/MainDoorTrigger.cs
+++ Assets/Scripts/Doors/MainDoorTrigger.cs
@@ -5,9 +5,27 @@ public class MainDoorTrigger : MonoBehaviour
 {
 	//[HideInInspector]
 	public bool isActivated;
-
+    public Light[] lights;
+    float intensity;
 	void Start()
 	{
-		isActivated = false;
+        lights = GetComponentsInChildren<Light>();
+        intensity = lights[0].intensity;
+        foreach (Light light in lights)
+        {
+            light.intensity = 0;
+        }
+        isActivated = false;
 	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (!isActivated)
+        {
+            foreach (Light light in lights)
+            {
+                light.intensity = intensity;
+            }
+        }
+    }
 }
